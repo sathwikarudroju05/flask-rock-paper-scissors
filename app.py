e2dbccd848b1b8462cb86785c33159a4e2713804
sathwikarudroju05 <rudrojusathwika@gmail.com>
2025-12-27 23:30:42 +0530

    Fix Flask template and static folder paths

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
-
 from flask import Flask, render_template, request, redirect, session
 import sqlite3
 import random
 import os
 from werkzeug.security import generate_password_hash, check_password_hash
 
-app = Flask(__name__)
+app = Flask(
+    __name__,
+    template_folder="templates",
+    static_folder="static"
+)
 
 # ---------- SECRET KEY ----------
 app.secret_key = os.environ.get("SECRET_KEY", "fallback-secret")
+
 
 # ---------- DATABASE SETUP ----------
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -156,4 +160,3 @@ def logout():
 
 if __name__ == "__main__":
     app.run()
->>>>>>> e438721aaa463f48684c4deb06ab523feb6fed5d
